d4f6edf1b3933adc1b10736ab08fc8b111940aec
abdul hamid <abdulachik@icloud.com>
2022-12-15 01:43:38 -0600

    Updating outdated code

--- rest_framework_tus/urls.py
+++ rest_framework_tus/urls.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path
+
 from rest_framework_tus.views import UploadViewSet
 
 from .routers import TusAPIRouter
@@ -8,6 +10,6 @@ router = TusAPIRouter()
 router.register(r'files', UploadViewSet, basename='upload')
 
 urlpatterns = [
-    url(r'', include((router.urls, 'rest_framework_tus'), namespace='api'))
+    path('/', include((router.urls, 'rest_framework_tus'), namespace='api'))
 ]
 app_name = 'rest_framework_tus'
