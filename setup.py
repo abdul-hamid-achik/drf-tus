6be48eb19e44333dfc97b86409434e28332b78c9
abdul hamid <abdulachik@icloud.com>
2022-12-15 01:37:15 -0600

    F

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         'Django>=1.8,<=4.1',
         'djangorestframework>=3.5.4',
         'jsonfield>=2.0.0',
-        'django-fsm==2.7.0',
+        'django-fsm==2.8.0',
         'six>=1.11.0',
     ],
     license="MIT",
